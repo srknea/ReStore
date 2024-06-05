11e2cb803a0f0d5063f92d31665bd0dfc0d51145
Serkan IŞIK <dehnas79@gmail.com>
2024-06-05 12:38:11 +0300

    Using react hooks - useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
+
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'Product 1', price: 100},
+    {name: 'Product 2', price: 200},
+  ]);
+
+  function addProduct(){
+    setProducts(prevState => [...prevState, 
+      {name: 'Product ' + (prevState.length + 1), price: (prevState.length * 100) + 100}
+    ]);
+  }
+
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   )
 }
