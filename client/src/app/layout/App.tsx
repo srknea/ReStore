6cb3028479804d709ab4ce6e96d0990ebcb43531
Serkan IŞIK <dehnas79@gmail.com>
2024-06-17 16:06:38 +0300

    Using Material UI Theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,24 @@
-import { useState, useEffect } from "react";
-import {Product} from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, []);
-
-  function addProduct(){
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 999,
-        name: 'Product ' + (prevState.length + 1), 
-        description: 'This is a product description',
-        price: (prevState.length * 100) + 100,
-        pictureUrl: 'https://via.placeholder.com/150',
-      }
-    ]);
-  }
-
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+  }});
+    
   return (
-    <>
+
+    <ThemeProvider theme={theme}>
       <CssBaseline/> 
       <Header/>
       <Container>
-        <Catalog products={products} addProduct = {addProduct}/>
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
+
   )
 }
 
