55480d88fe665c6f9672d233172db8a311331939
Serkan IŞIK <dehnas79@gmail.com>
2024-06-17 18:59:50 +0300

    setting bg color

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -10,6 +10,9 @@ function App() {
   const theme = createTheme({
     palette: {
       mode: palletType,
+      background:{
+        default: palletType === "light" ? "#eaeaea" : "#121212"
+      }
   }});
 
   function handleThemeChange(){
